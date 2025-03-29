21a18a7cee4e28c79198902626477d317efefdbd
DESKTOP-5SQT41E\KHANGVU <vuhuychuyen1992@gmail.com>
2025-03-30 01:26:05 +0700

    feat: add user authentication

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,20 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'ci',
-        'chore',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test',
-      ],
-    ],
-  },
+    'type-enum': [0, 'always', [
+      'build', 'ci', 'chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'
+    ]],
+    'type-case': [0, 'always', 'lower-case'],
+    'type-empty': [0, 'never'],
+    'scope-enum': [0, 'always', ['api', 'ui', 'core', 'docs']],
+    'scope-case': [0, 'always', 'lower-case'],
+    'scope-empty': [0, 'never'],
+    'subject-case': [0, 'always', 'sentence-case'],
+    'subject-empty': [0, 'never'],
+    'subject-full-stop': [0, 'never', '.'],
+    'header-max-length': [0, 'always', 72],
+    'body-max-line-length': [0, 'always', 100],
+    'footer-max-length': [0, 'always', 100],
+    'signed-off-by': [0, 'always', 'Signed-off-by'],
+  }
 };
